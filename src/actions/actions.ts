b747ec5135d35bdfb1eeba2706fc676a9f024b28
rahman <rahmanqadirzade@gmail.com>
2020-03-12 20:27:42 +0300

    'last'

---
+++ src/actions/actions.ts
@@ -0,0 +1,12 @@
+import { action } from "typesafe-actions";
+
+// use typescript enum rather than action constants
+export enum actionTypes {
+  ADD = "ADD",
+  DELETE = "DELETE"
+}
+
+export const todoActions = {
+  add: (item: string) => action(actionTypes.ADD, item),
+  delete: (idx: number) => action(actionTypes.DELETE, idx)
+};
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: "production",
+
+  entry: "./src/index.tsx",
+
+  output: {
+    path: __dirname + "/build",
+    publicPath: "build/",
+    filename: "bundle.js"
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
